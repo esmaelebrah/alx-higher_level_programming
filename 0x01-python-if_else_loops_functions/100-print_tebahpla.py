754bf819caa032f8c3e9a19df3d851cb361fadf3
esmaelebrah <esmaelebrahimhasen@gmail.com>
2023-05-09 15:04:29 -0700

    updated

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for i in range(122, 96):
-    print("{:c}".format(str(i) if i % 2 == 0 else str(i + 65 - 97)), end="")
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{:c}".format((c - (ord('a') - ord('A'))) if c % 2 else c), end='')
