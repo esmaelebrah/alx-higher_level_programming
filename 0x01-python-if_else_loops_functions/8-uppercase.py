fdcc55ff101581b86394356710afc5e34d65c4ef
esmaelebrah <esmaelebrahimhasen@gmail.com>
2023-05-09 13:20:57 -0700

    updated

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    new_str = ""
+    for ch in str:
+        if ord(ch) >= 97 and ord(ch) <= 122:
+            ch = chr(ord(ch) + 65 -97)
+            new_str += ch
+        else:
+            new_str += ch
+    print("{:s}".format(new_str))
