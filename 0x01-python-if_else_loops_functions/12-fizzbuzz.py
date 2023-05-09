a535282192f9a0bf066ee2b75773cd7d3ca6ba9e
esmaelebrah <esmaelebrahimhasen@gmail.com>
2023-05-09 14:25:23 -0700

    updated

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -6,6 +6,6 @@ def fizzbuzz():
         if i % 5 == 0:
             print("Buzz", end=" ")
         if i % 3 == 0:
-            print("Fizz", end="")
+            print("Fizz", end=" ")
         else:
             print(i, end=" ")
