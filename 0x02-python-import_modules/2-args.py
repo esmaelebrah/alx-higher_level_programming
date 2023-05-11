7cbdca77bf6d1bc7d58090a5df4179891faffd4d
esmaelebrah <esmaelebrahimhasen@gmail.com>
2023-05-11 13:07:05 -0700

    updated

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,4 +4,4 @@ if __name__ == "__main__":
     length = len(sys.argv) - 1
     print("{:d} {:s}{}".format(length, "argument" if length == 1 else "arguments", "." if length == 0 else ":"))
     for i in range(1, length + 1):
-        print("{:}: {:s}".format(i, sys.argv[i]))
+        print("{:d}: {:s}".format(i, sys.argv[i]))
