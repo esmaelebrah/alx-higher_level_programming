a9ec07a040395684bfee6f1b31de14be86b960a6
esmaelebrah <esmaelebrahimhasen@gmail.com>
2023-05-11 14:29:55 -0700

    updated

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
-
+if __name__ == "__main__":
+    import hidden_4
+    list = dir(hidden_4)
+    for i in range(len(list)):
+        if list[i][:2] != "__":
+            print("{:s}".format(list[i]))
