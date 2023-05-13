2a4aa38327670345b4db93ccf098f567f22e51c5
esmaelebrah <esmaelebrahimhasen@gmail.com>
2023-05-13 12:32:29 -0700

    fith commit

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
-
+def new_in_list(my_list, idx, element):
+    if idx < 0:
+        new_list = my_list
+    elif idx >= len(my_list):
+        new_list = my_list
+    else:
+        new_list = []
+        for i in range(len(my_list)):
+            if i == idx:
+                new_list.append(element)
+            else:
+                new_list.append(my_list[i])
+    return new_list
