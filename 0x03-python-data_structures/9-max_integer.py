2df7c0028581541bca76544ac23491649015cc07
esmaelebrah <esmaelebrahimhasen@gmail.com>
2023-05-14 12:43:09 -0700

    updated

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -4,7 +4,7 @@ def max_integer(my_list=[]):
         return None
     else:
         maxm = my_list[0]
-        for i in range(len(my_list)):
-                if my_list[i] > maxm:
-                    maxm = my_list[i]
+        for i in range(1, len(my_list)):
+            if my_list[i] > maxm:
+                maxm = my_list[i]
         return maxm
