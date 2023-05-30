413d617a1a0014c82e4e30a3e0970000f110b855
esmaelebrah <esmaelebrahimhasen@gmail.com>
2023-05-30 07:56:47 -0700

    first commit

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""define a square class"""
+
+
+class Square:
+    """private atribute
+    __size 
+    size of side of swuare"""
+    def __init__(self, size):
+        self.__size = size
