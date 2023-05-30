6e9ead5af8833349f75207b7d7f8b87cfae339ec
esmaelebrah <esmaelebrahimhasen@gmail.com>
2023-05-30 08:03:07 -0700

    first commit

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
-"""define a square class"""
+"""Square class definition"""
 
 
 class Square:
-    """private atribute
-    __size 
-    size of side of swuare
+    """Represents a square
+
+    Attributes:
+        __size (int): size of a side of the square
     """
     def __init__(self, size):
-        """insialize the sixw
-        return:None
+        """Initializes a square
+
+        Args:
+            size (int): size of a side of the square
+
+        Returns: None
         """
         self.__size = size
