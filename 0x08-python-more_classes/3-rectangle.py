8805ab21d7d104985a1d0ff73529014659d24cf5
esmaelebrah <esmaelebrahimhasengmail.com>
2023-06-05 23:38:10 +0300

    updated

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -51,7 +51,7 @@ class Rectangle:
 
     def __str__(self):
         """returns printable string representation of the rectangle"""
-        st = ""
+        st = "\n"
         if self.__width != 0 and self.__height != 0:
             for i in range(self.__height):
                 for j in range(self.__width):
