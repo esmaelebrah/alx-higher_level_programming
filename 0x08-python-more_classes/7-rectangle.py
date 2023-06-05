e20a3d3fecc11107b79b2f77d945255ca8eedbeb
esmaelebrah <esmaelebrahimhasen@gmail.com>
2023-06-05 15:25:46 -0700

    updated

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -65,7 +65,7 @@ class Rectangle:
         if self.__width != 0 and self.__height != 0:
             for i in range(self.__height):
                 for j in range(self.__width):
-                    st += "#"
+                    st += self.print_symbol
                 if i < self.__height - 1:
                     st += "\n"
         return st
